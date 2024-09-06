cd366dc00fe8a0f4448aec1248986f6a9d05814b
Mubashir12392 <mubashirhaider01@gmail.com>
2024-09-06 06:06:10 +0500

    Write a class based view of Bookdetailview

--- UnitTesting/apitesting/views.py
+++ UnitTesting/apitesting/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework import status
+
 import logging
 logger = logging.getLogger('django')
 
@@ -31,26 +32,30 @@ class BookListView(APIView):
 
 
 class BookDetailView(APIView):
+    try:
 
-    def get(self, request):
+        def get(self, request):
 
-        pk = request.query_params.get('pk')
-        book = get_object_or_404(Book, pk=pk)
-        serializer = BookSerializer(book)
-        return JsonResponse(serializer.data, status=status.HTTP_200_OK)
-    
-    def put(self, request):
-
-        pk = request.query_params.get('pk')
-        book = get_object_or_404(Book, pk=pk)
-        serializer = BookSerializer(book, request.data)
-        if serializer.is_valid():
-            serializer.save()
+            pk = request.query_params.get('pk')
+            book = get_object_or_404(Book, pk=pk)
+            serializer = BookSerializer(book)
             return JsonResponse(serializer.data, status=status.HTTP_200_OK)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request):
-        pk = request.query_params.get('pk')
-        book = get_object_or_404(Book, pk=pk)
-        book.delete()
-        return JsonResponse({'message':'book deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+        
+        def put(self, request):
+
+            pk = request.query_params.get('pk')
+            book = get_object_or_404(Book, pk=pk)
+            serializer = BookSerializer(book, request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        def delete(self, request):
+            pk = request.query_params.get('pk')
+            book = get_object_or_404(Book, pk=pk)
+            book.delete()
+            return JsonResponse({'message':'book deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+    except Exception as err:
+        logger.error('error in BookDetailView' + str(err))
+        raise Response.internal_server_error(err)
