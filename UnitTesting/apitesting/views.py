1213091f905308e32720ea85ea6af4570bd513e3
Mubashir12392 <mubashirhaider01@gmail.com>
2024-09-25 02:49:40 +0500

    remove book detail view

--- UnitTesting/apitesting/views.py
+++ UnitTesting/apitesting/views.py
@@ -23,7 +23,7 @@ class BookListView(APIView):
             book = BookSerializer(request.data)
             if book.is_valid():
                 book.save()
-                return JsonResponse(book.data, status=status.HTTP_201_CREATED)
+                return JsonResponse(book.data, status=status.HTTP_201_CREATED )
             return JsonResponse(book.errors, status=status.HTTP_400_BAD_REQUEST)
         
     except Exception as err:
@@ -31,31 +31,3 @@ class BookListView(APIView):
         raise Response.internal_server_error(err)
 
 
-class BookDetailView(APIView):
-    try:
-
-        def get(self, request):
-
-            pk = request.query_params.get('pk')
-            book = get_object_or_404(Book, pk=pk)
-            serializer = BookSerializer(book)
-            return JsonResponse(serializer.data, status=status.HTTP_200_OK)
-        
-        def put(self, request):
-
-            pk = request.query_params.get('pk')
-            book = get_object_or_404(Book, pk=pk)
-            serializer = BookSerializer(book, request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return JsonResponse(serializer.data, status=status.HTTP_200_OK)
-            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-        def delete(self, request):
-            pk = request.query_params.get('pk')
-            book = get_object_or_404(Book, pk=pk)
-            book.delete()
-            return JsonResponse({'message':'book deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
-    except Exception as err:
-        logger.error('error in BookDetailView' + str(err))
-        raise Response.internal_server_error(err)
